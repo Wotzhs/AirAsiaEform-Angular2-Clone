50e2a5180297780209e070c8b021a782ceb30efc
Wong Tze Hsiung <wotzhs@gmail.com>
2016-11-08 17:22:57 +0800

    added footer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
-import { FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeaderComponent} from './components/header/header.component';
 import { LanguageComponent} from './components/language/language.component';
 import { FormComponent} from './components/form/form.component';
+import { FooterComponent} from './components/footer/footer.component';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HeaderComponent, LanguageComponent, FormComponent],
+  imports: [BrowserModule],
+  declarations: [AppComponent, HeaderComponent, LanguageComponent, FormComponent, FooterComponent],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
